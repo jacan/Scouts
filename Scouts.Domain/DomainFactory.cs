3afc804c11ad82ad6e1d86d56b11bf35a57a70e2
jacan <jckandersen@gmail.com>
2017-08-17 13:37:25 +0200

    New domain objects

---
+++ Scouts.Domain/DomainFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Scouts.Domain
+{
+    /// <summary>
+    /// Base level domain factory
+    /// </summary>
+    public static class DomainFactory
+    {
+        public static DomainId CreateId()
+        {
+            return new DomainId(Guid.NewGuid());
+        }
+    }
+}
---
+++ Scouts.Domain/Schedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Scouts.Domain
+{
+    public class Schedule
+    {
+        public Schedule(DateTime startDate, DateTime endDate)
+        {
+            StartDate = startDate.Date;
+            EndDate = endDate.Date;
+        }
+
+        public DateTime StartDate { get; }
+        public DateTime EndDate { get; }
+    }
+}
